bb74de373b52d39bcff67c100ece8096696f97a8
Steven Smith <sos22@cam.ac.uk>
2014-04-05 21:15:45 -0700

    Actually stash incoming messages in a message pool area.

--- Harbinger.hs
+++ Harbinger.hs
@@ -4,6 +4,13 @@ import Control.Monad.Writer.Lazy
 import qualified Data.ByteString.Lazy as BSL
 import Data.Word
 import Data.Char
+import System.IO
+import System.Random
+import Network.BSD
+import Data.Time
+import Data.Time.Format
+import System.Locale
+import System.Directory
 
 type Errorable = Writer [String]
 
@@ -13,17 +20,44 @@ runErrorable x = case runWriter x of
   (_, r) -> Left r
 
 data Header = Header String String deriving Show
+data Email = Email { eml_headers :: [Header], eml_body :: BSL.ByteString } deriving Show
+
+data Journal = Journal { journal_handle :: Handle,
+                         journal_path :: FilePath }
+data JournalEntry = JournalStart String
+                    deriving Show
+
+openCreate :: FilePath -> IO (Maybe Handle)
+openCreate path = do exists <- doesFileExist path
+                     if exists
+                       then return Nothing
+                       else liftM Just $ openFile path WriteMode
+
+openMessageJournal :: IO Journal
+openMessageJournal =
+  let journalDir = "harbinger/journal"
+      journalPath cntr = journalDir ++ "/" ++ (show cntr)
+      tryFrom cntr = let p = journalPath cntr
+                     in do ee <- openCreate p
+                           case ee of
+                             Nothing -> tryFrom $ cntr + 1
+                             Just hh -> return $ Journal { journal_handle = hh, journal_path = p }
+  in (createDirectoryIfMissing True journalDir) >> (tryFrom 0)
+
+journalWrite :: Journal -> JournalEntry -> IO ()
+journalWrite journal je =
+  let h = journal_handle journal in
+  case je of
+    JournalStart msgId -> hPutStrLn h $ "start " ++ msgId
+      
 
 parseAscii7 :: [Word8] -> String
 parseAscii7 = map $ chr . fromInteger . toInteger
 
-stateSequence :: [a] -> [(a, ([a], [a]))]
-stateSequence elems =
-  zip elems $ zip (inits elems) (tails $ tail elems)
-
 toSep :: Eq a => a -> [a] -> Maybe ([a], [a])
 toSep sep elems =
-  case find (\(a, _) -> a == sep) $ stateSequence elems of
+  let stateSequence elems = zip elems $ zip (inits elems) (tails $ tail elems)
+  in case find (\(a, _) -> a == sep) $ stateSequence elems of
     Nothing -> Nothing
     Just x -> Just $ snd x
 
@@ -80,25 +114,100 @@ unfoldHeaderLines what =
     (Nothing, res) -> res
     (Just c, res) -> c:res
 
-parseHeader :: String -> Errorable Header
-parseHeader hdrLine =
+extractHeader :: String -> Errorable Header
+extractHeader hdrLine =
   case toSep ':' hdrLine of
     Nothing -> tell ["No : in header line " ++ hdrLine] >> (return $ Header hdrLine "")
     Just (a, b) -> return $ Header a (dropWhile (flip elem " \t") b)
-  
-parseEmail :: BSL.ByteString -> Errorable ([Header], BSL.ByteString)
+
+parseEmail :: BSL.ByteString -> Errorable Email
 parseEmail what =
   do (headerLines, body) <- headersBody what
-     parsedHeaders <- mapM parseHeader $ unfoldHeaderLines headerLines
-     return (parsedHeaders, body)
+     parsedHeaders <- mapM extractHeader $ unfoldHeaderLines headerLines
+     return $ Email parsedHeaders body
 
 stringToByteString :: String -> BSL.ByteString
 stringToByteString = BSL.pack . map (fromInteger . toInteger . ord)
 
+flattenHeader :: Header -> BSL.ByteString
+flattenHeader (Header name value) = BSL.concat $ map stringToByteString [name, ": ", value, "\r\n"]
+  
+flattenEmail :: Email -> BSL.ByteString
+flattenEmail eml =
+  BSL.append (BSL.concat $ map flattenHeader (eml_headers eml)) $ BSL.append (stringToByteString "\r\n") $ eml_body eml
+
+getHeader :: String -> Email -> Maybe String
+getHeader name eml =
+  lookup (map toLower name) $ map (\(Header x y) -> (map toLower x,y)) $ eml_headers eml
+
+addHeader :: Header -> Email -> Email
+addHeader hdr eml = eml { eml_headers = hdr:(eml_headers eml) }
+
+removeHeader :: String -> Email -> Email
+removeHeader hdr eml = eml { eml_headers = filter (\(Header name _) -> (map toLower name) /= (map toLower hdr)) $ eml_headers eml }
+
+ensureMessageId :: Email -> IO Email
+ensureMessageId eml =
+  case getHeader "Message-Id" eml of
+    Just _ -> return eml
+    Nothing ->
+      do {- Pick a range big enough to be statistically unique -}
+         ident <- getStdRandom $ randomR (0::Integer,1000000000000000)
+         hostName <- getHostName
+         let newIdent = (show ident) ++ "@" ++ hostName ++ "-harbinger"
+         return $ eml {eml_headers = (Header "Message-Id" newIdent):(eml_headers eml)}
+
+addReceivedDate :: Email -> IO (Email, UTCTime)
+addReceivedDate eml =
+  do now <- Data.Time.getCurrentTime
+     let fmtTime = Data.Time.Format.formatTime System.Locale.defaultTimeLocale "%F %T" now
+     return (addHeader (Header "X-Harbinger-Received" fmtTime) eml,
+             now)
+
+sanitiseForPath :: String -> String
+sanitiseForPath = filter $ flip elem "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,^@%"
+
+emailPoolFile :: Email -> (String, String)
+emailPoolFile eml =
+  case fmap sanitiseForPath $ getHeader "Message-ID" eml of
+    Nothing -> error "message has no ID?"
+    Just msgId -> let poolDir = "harbinger/pool/" ++ (take 5 msgId)
+                      poolFile = poolDir ++ "/" ++ msgId
+                  in (poolDir, poolFile)
+
+deMaybe :: Maybe a -> a
+deMaybe (Just x) = x
+deMaybe Nothing = error "Maybe wasn't?"
+
+fileEmail :: Email -> IO ()
+fileEmail eml =
+  do (eml', when) <- ensureMessageId eml >>= addReceivedDate
+     let (_, poolFile) = emailPoolFile eml'
+     conflict <- doesFileExist poolFile
+     eml'' <- if conflict
+              then ensureMessageId $
+                   addHeader (Header "X-Harbinger-Old-Id" $ deMaybe $ getHeader "Message-Id" eml') $
+                   removeHeader "Message-Id" eml'
+              else return eml'
+     let (poolDir', poolFile') = emailPoolFile eml''
+     conflict' <- doesFileExist poolFile'
+     if conflict'
+       then error $ "Failed to generate unique message ID! (" ++ poolFile ++ ", " ++ poolFile' ++ ")"
+       else return ()
+     createDirectoryIfMissing True poolDir'
+     j <- openMessageJournal
+     journalWrite j (JournalStart $ deMaybe $ getHeader "Message-Id" eml'')
+     hh <- openFile poolFile' WriteMode
+     BSL.hPut hh $ flattenEmail eml''
+     hClose hh
+     hClose $ journal_handle j
+     removeFile $ journal_path j
+     
 main :: IO ()
 main =
-  do print $ runErrorable $ headersBody $ stringToByteString "H1\r\nH2\r\nH3\r\n\r\nbody"
-     print $ unfoldHeaderLines $ ["L1", " C1", " C2", "L2", "L3", "L4", " C3"]
-     print $ runErrorable $ parseEmail $ stringToByteString "From: foo\r\n bar\r\n\tbazz\r\nTo: Me\r\n\r\nBody"
-     print $ runErrorable $ parseEmail $ stringToByteString "From: foo\r\nHeader1\r\nHeader2\r\n\r\nFoo"
+  do parsed <- liftM (runErrorable . parseEmail) BSL.getContents
+     case parsed of
+       Left errs -> print errs
+       Right parsed' -> fileEmail parsed'
+     
 
