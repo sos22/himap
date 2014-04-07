c5fc670a132a6be8b0a77e9210e2f69173200683
Steven Smith <sos22@cam.ac.uk>
2014-04-06 21:56:57 -0700

    Start work on an actual IMAP command parser.

--- Imap.hs
+++ Imap.hs
@@ -5,51 +5,49 @@ import Control.Concurrent
 import Control.Exception
 import Control.Monad
 import Data.Char
+import Data.IORef
+import Data.Word
 import Debug.Trace
-
-instance Functor (Either a) where
-  fmap f (Right x) = Right $ f x
-  fmap _ (Left x) = Left x
-  
-second :: (a -> b) -> (c, a) -> (c, b)
-second f (x, y) = (x, f y)
 
 ignore :: Monad m => m a -> m ()
 ignore = flip (>>) $ return ()
 
 stringToByteString :: String -> BS.ByteString
 stringToByteString = BS.pack . map (fromInteger.toInteger.ord)
-byteStringToString :: BS.ByteString -> String
-byteStringToString = map (chr.fromInteger.toInteger) . BS.unpack
-bsEOL :: BS.ByteString
-bsEOL = stringToByteString "\r\n"
 
 data ImapServerState = ImapServerState { iss_handle :: Handle, 
                                          iss_outgoing_response :: [BS.ByteString],
-                                         iss_incoming_buffer :: BS.ByteString
+                                         iss_inbuf :: IORef BS.ByteString,
+                                         iss_inbuf_idx :: Int
                                          }
 
-data ImapServer a = ImapServer { run_is :: ImapServerState -> IO (Maybe (ImapServerState, a)) }
+data ImapStopReason = ImapStopFinished
+                    | ImapStopFailed String
+                    | ImapStopBacktrack
+                      deriving Show
+data ImapServer a = ImapServer { run_is :: ImapServerState -> IO (Either ImapStopReason (ImapServerState, a)) }
 instance Monad ImapServer where
-  return x = ImapServer $ \state -> return $ Just (state, x)
+  return x = ImapServer $ \state -> return $ Right (state, x)
   first >>= secondF = ImapServer $ \state ->
     do firstRes <- run_is first state
        case firstRes of
-         Nothing -> return Nothing
-         Just (newState, firstRes') ->
+         Left r -> return $ Left r
+         Right (newState, firstRes') ->
            run_is (secondF firstRes') newState
            
 runImapServer :: Handle -> ImapServer () -> IO ()
 runImapServer hndle is =
-  ignore $ run_is is $ ImapServerState { iss_handle = hndle, 
-                                         iss_outgoing_response = [],
-                                         iss_incoming_buffer = BS.empty }
+  do inbuf <- newIORef BS.empty
+     ignore $ run_is is $ ImapServerState { iss_handle = hndle, 
+                                            iss_outgoing_response = [],
+                                            iss_inbuf = inbuf, 
+                                            iss_inbuf_idx = 0}
 
 queueResponse :: String -> ImapServer ()
 queueResponse what = ImapServer $ \isState ->
-  return $ Just (isState {iss_outgoing_response = (stringToByteString what):
-                                                  (iss_outgoing_response isState)},
-                 ())
+  return $ Right (isState {iss_outgoing_response = (stringToByteString what):
+                                                   (iss_outgoing_response isState)},
+                  ())
 
 finishResponse :: ImapServer ()
 finishResponse = ImapServer $ \isState ->
@@ -57,7 +55,7 @@ finishResponse = ImapServer $ \isState ->
       worker (x:xs) = worker xs >> BS.hPut (iss_handle isState) x
   in do worker (iss_outgoing_response isState)
         hFlush (iss_handle isState)
-        return $ Just (isState {iss_outgoing_response = []}, ())
+        return $ Right (isState {iss_outgoing_response = []}, ())
 
 data ResponseTag = ResponseUntagged
                  | ResponseTagged String
@@ -72,15 +70,10 @@ data ResponseAttribute = ResponseAttribute
 attrToString :: ResponseAttribute -> String
 attrToString = error "No attributes yet"
 
-data ImapVerb = ImapNoop
-              | ImapCapability
-              | ImapBadCommand
-              deriving Show
-data ImapArg = ImapArg String
-             deriving Show
-data ImapCommand = ImapCommand { imc_tag :: ResponseTag,
-                                 imc_verb :: ImapVerb,
-                                 imc_args :: [ImapArg] }
+data ImapCommand = ImapNoop
+                 | ImapCapability
+                 | ImapLogin String String
+                 | ImapCommandBad String
                    deriving Show
      
 sendResponse :: ResponseTag -> ResponseState -> [ResponseAttribute] -> String -> ImapServer ()
@@ -110,107 +103,160 @@ sendResponseOk t = sendResponse t ResponseStateOk
 sendResponseBad :: ResponseTag -> [ResponseAttribute] -> String -> ImapServer ()
 sendResponseBad t = sendResponse t ResponseStateBad
 
-data Parser tokens res = Parser { run_parser :: [tokens] -> Either String ([tokens], res) }
+lookaheadByte :: ImapServer Word8
+lookaheadByte = ImapServer worker
+  where worker state =
+          do inbuf <- readIORef $ iss_inbuf state
+             if iss_inbuf_idx state >= BS.length inbuf
+               then do nextChunk <- BS.hGetSome (iss_handle state) 4096
+                       if BS.length nextChunk == 0
+                         then return $ Left ImapStopFinished
+                         else do modifyIORef (iss_inbuf state) (flip BS.append nextChunk)
+                                 worker state
+               else return $ Right (state, BS.index inbuf (iss_inbuf_idx state))
 
-instance Monad (Parser tokens) where
-  return x = Parser $ \tokens -> Right (tokens, x)
-  first >>= secondF = Parser $ \tokens ->
-    case run_parser first tokens of
-      Left err -> Left err
-      Right (leftover, res1) ->
-        run_parser (secondF res1) leftover
+readByte :: ImapServer Word8
+readByte = lookaheadByte >>= \res ->
+  ImapServer $ \state -> return $ Right (state {iss_inbuf_idx = (iss_inbuf_idx state) + 1},
+                                         res)
+                                     
+readCountedBytes :: Int -> ImapServer [Word8]
+readCountedBytes 0 = return []
+readCountedBytes x =
+  do c <- readByte
+     c' <- readCountedBytes (x - 1)
+     return $ c:c'
+
+readCountedChars :: Int -> ImapServer String
+readCountedChars n = liftM (map (chr.fromInteger.toInteger)) $ readCountedBytes n
+
+readChar :: ImapServer Char
+readChar = liftM (chr.fromInteger.toInteger) readByte
+
+lookaheadChar :: ImapServer Char
+lookaheadChar = liftM (chr.fromInteger.toInteger) lookaheadByte
+
+parseBacktrack :: ImapServer a
+parseBacktrack = ImapServer $ \_ -> return $ Left ImapStopBacktrack
+
+alternates :: [ImapServer a] -> ImapServer a
+alternates options = ImapServer $ worker options
+  where worker [] _ = return $ Left $ ImapStopBacktrack
+        worker (opt1:opts) state =
+          do res1 <- run_is opt1 state
+             case res1 of
+               Left ImapStopBacktrack -> worker opts state
+               Left err -> return $ Left err
+               Right res2 -> return $ Right res2
         
-runParser :: Parser tokens a -> [tokens] -> Either String a
-runParser parser string = fmap snd $ run_parser parser string
+runWithErrs :: ImapServer a -> ImapServer (Either String a)
+runWithErrs underlying = ImapServer $ \state ->
+  do res <- run_is underlying state
+     return $ Right $ case res of
+       Left (ImapStopFailed errMsg) -> (state, Left errMsg)
+       Left (ImapStopFinished) -> (state, Left "reached the end")
+       Left (ImapStopBacktrack) -> (state, Left "ran out of backtrack")
+       Right (state', x) -> (state', Right x)
+       
+imapParserFlush :: ImapServer ()
+imapParserFlush = ImapServer $ \state -> do modifyIORef (iss_inbuf state) (BS.drop (iss_inbuf_idx state))
+                                            return $ Right (state { iss_inbuf_idx = 0 }, ())
 
-parserTakeWhile :: (a -> Bool) -> Parser a [a]
-parserTakeWhile predicate =
-  Parser pp where pp = \tokens ->
-                    case tokens of
-                      [] -> Right ([], [])
-                      (tok:others) | predicate tok ->
-                        fmap (second $ (:) tok) $ pp others
-                      leftover -> Right (leftover, [])
-
-parserSplitOn :: (a -> Bool) -> Parser a [[a]]
-parserSplitOn predicate =
-  Parser (\tokens ->
-           case foldr
-                (\tok (line, otherLines) -> if predicate tok
-                                            then ([], line:otherLines)
-                                            else (tok:line, otherLines))
-                ([], [])
-                tokens of
-             (l, res) -> Right ([], l:res))
-  
-parseThis :: Eq a => a -> Parser a ()
-parseThis what = Parser $ \tokens -> case tokens of
-  [] -> Left "Unexpected EOF"
-  (x:xs) | what == x -> Right (xs, ())
-         | otherwise -> Left "parseThis failed"
-                        
-infixr 1 <.>
-(<.>) :: Parser Char a -> Parser Char b -> Parser Char (a, b)
-(<.>) parser1 parser2 =
-  do r1 <- parser1
-     r2 <- parser2
-     return (r1, r2)
-
-orEof :: Parser c a -> Parser c (Maybe a)
-orEof parser = Parser $ \tokens -> case tokens of
-  [] -> Right ([], Nothing)
-  _ -> fmap (second Just) $ run_parser parser tokens
-
-parseTag :: Parser Char ResponseTag
-parseTag = do pp <- parserTakeWhile $ (/=) ' '
-              parseThis ' '
-              return $ if pp == "*"
-                       then ResponseUntagged
-                       else ResponseTagged pp
-
-parseVerb :: Parser Char ImapVerb
-parseVerb = liftM worker $ parserTakeWhile $ (/=) ' '
-            where worker "NOOP" = ImapNoop
-                  worker "CAPABILITY" = ImapCapability
-                  worker _ = ImapBadCommand
-
-parseArgs :: Parser Char [ImapArg]
-parseArgs = (parseThis ' ') >> (liftM (map ImapArg) $ parserSplitOn $ (==) ' ')
-
-parseIncomingCommand :: String -> Either String ImapCommand
-parseIncomingCommand s =
-  case runParser (parseTag <.> parseVerb <.> (orEof parseArgs)) s of
-    Left err -> Left err
-    Right (tag, (verb, args)) -> Right $ ImapCommand tag verb (maybe [] id args)
-
-processCommand :: (Either String ImapCommand) -> ImapServer ()
-processCommand (Left err) = sendResponseBad ResponseUntagged [] $ "Cannot parse command: " ++ err
-processCommand (Right cmd) =
-  trace ("Run command " ++ (show cmd)) $
-  case imc_verb cmd of
-    ImapCapability -> do sendResponse ResponseUntagged ResponseStateNone [] "CAPABILITY IMAP4rev1"
-                         sendResponseOk (imc_tag cmd) [] "Done capability"
-    ImapNoop -> sendResponseOk (imc_tag cmd) [] "Done noop"
-    ImapBadCommand -> sendResponseBad (imc_tag cmd) [] "Bad command"
-
-readCommand :: ImapServer (Either String ImapCommand)
+skipToEOL :: ImapServer String
+skipToEOL = do c <- readChar
+               if c == '\r'
+                 then state1
+                 else liftM ((:) c) skipToEOL
+            where state1 = do c <- readChar
+                              if c == '\n'
+                                then return ""
+                                else if c == '\r'
+                                     then state1
+                                     else liftM ((:) c) skipToEOL
+                                          
+readCommand :: ImapServer (ResponseTag, ImapCommand)
 readCommand =
-  ImapServer worker
-  where worker isState =
-          case BS.findSubstring bsEOL $ iss_incoming_buffer isState of
-            Just idx -> let (thisCommand, newBuf) = BS.splitAt idx $ iss_incoming_buffer isState
-                        in return $ Just $ (isState { iss_incoming_buffer = BS.drop 2 newBuf },
-                                            parseIncomingCommand $ byteStringToString thisCommand)
-            Nothing ->
-              do nextFrag <- BS.hGetSome (iss_handle isState) 4096
-                 if BS.length nextFrag == 0
-                   then return Nothing
-                   else worker (isState { iss_incoming_buffer = BS.append (iss_incoming_buffer isState) nextFrag })
+  do t <- liftM ResponseTagged readTag
+     requireChar ' '
+     cmd <- alternates [alternates $ map (\(name, x) -> do requireString name
+                                                           r <- x
+                                                           requireEOL
+                                                           return r) commandParsers,
+                        do l <- skipToEOL
+                           return $ ImapCommandBad l
+                       ]
+     imapParserFlush
+     return (t, cmd)
+  where readTag = alternates [do c <- readChar
+                                 if c == ' '
+                                   then parseBacktrack
+                                   else liftM ((:) c) readTag,
+                              return ""]
+        requireChar c = do c' <- readChar
+                           if c == c'
+                             then return ()
+                             else parseBacktrack
+        requireString = sequence_ . (map requireChar)
+        requireEOL = requireString "\r\n"
+        parseAstring = alternates [parseQuoted, parseLiteral, parseMany1 parseAstringChar]
+        parseQuoted = requireChar '"' >> worker
+          where worker = do c <- readChar
+                            case c of
+                              '"' -> return ""
+                              '\\' -> do c' <- readChar
+                                         liftM ((:) c') worker
+                              _ -> liftM ((:) c) worker
+        parseNumber = worker 0
+                      where worker acc =
+                              do c <- lookaheadChar
+                                 if not (c `elem` "1234567890")
+                                   then return acc
+                                   else do _ <- readChar
+                                           worker $ (acc * 10) + (digitToInt c)
+        parseLiteral = do requireChar '{'
+                          cnt <- parseNumber
+                          requireString "}\r\n"
+                          readCountedChars cnt
+        parseMany1 what = do r <- what
+                             alternates [do res <- parseMany1 what
+                                            return $ r:res,
+                                         return []]
+        parseAstringChar = do c <- readChar
+                              if (c `elem` "(){ %*\"\\") || (ord c < 32)
+                                then parseBacktrack
+                                else return c
+        commandParsers = [("NOOP", return ImapNoop),
+                          ("CAPABILITY", return ImapCapability),
+                          ("LOGIN", do requireChar ' '
+                                       username <- parseAstring
+                                       requireChar ' '
+                                       password <- parseAstring
+                                       return $ ImapLogin username password)]
 
+processCommand :: Either String (ResponseTag, ImapCommand) -> ImapServer ()
+processCommand (Left err) =
+  trace ("Failed: " ++ err) $ sendResponseBad ResponseUntagged [] $ "Failed: " ++ err
+processCommand (Right (tag, cmd)) =
+  trace ("Run command " ++ (show cmd)) $
+  case cmd of
+    ImapCapability -> do sendResponse ResponseUntagged ResponseStateNone [] "CAPABILITY IMAP4rev1"
+                         sendResponseOk tag [] "Done capability"
+    ImapNoop -> sendResponseOk tag [] "Done noop"
+    ImapLogin username password -> trace ("login as " ++ username ++ ", password " ++ password) $
+                                   sendResponseOk tag [] "Logged in"
+    ImapCommandBad l -> sendResponseBad tag [] $ "Bad command " ++ l
+    
+untilError :: ImapServer a -> ImapServer ()
+untilError server = ImapServer $ \state ->
+  do r <- run_is server state
+     case r of
+       Left st -> return $ Left st
+       Right (state', _) -> run_is (untilError server) state'
+       
 processClient :: Handle -> IO ()
 processClient clientHandle =
   runImapServer clientHandle $ do sendResponseOk ResponseUntagged [] "Hello"
-                                  forever (readCommand >>= processCommand)
+                                  untilError (runWithErrs readCommand >>= processCommand)
      
 main :: IO ()
 main =
